7b31dc83467d3d4b49d834df4aa52177ffde2f16
RobbeJansensPXL <robbe.jansens@student.pxl.be>
2022-02-28 17:08:10 +0100

    Initialized project

---
+++ app/src/main/java/be/pxl/android_vision_poc/MainActivity.kt
@@ -0,0 +1,11 @@
+package be.pxl.android_vision_poc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
